117b179246846d22e6ec995c99fc85beeb5c8f51
Wesley <wesleyadwebster@gmail.com>
2022-11-23 21:07:31 -0600

    Party slice of redux created

---
+++ src/app/partySlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    partyMembers: []
+}
+
+const partySlice = createSlice({
+    name: 'party',
+    initialState,
+    reducers: {
+        addPartyMember: (state, action) => {
+            state.party = action.payload;
+        }
+    }
+});
+
+
+export const editParty = (state) => state.party.partyMembers;
+
+export default partySlice.reducer;
--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import modalReducer from './modalSlice';
-import overlayReducer from './overlaySlice'
+import overlayReducer from './overlaySlice';
+import partyReducer from './partySlice';
 // import rootReducer from './reducers'
 
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
     overlay: overlayReducer,
+    party: partyReducer
   }
 })
