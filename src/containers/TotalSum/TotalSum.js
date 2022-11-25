4c1660f983e6540dbbb55d6b6fa62c037f3322b1
Wesley <wesleyadwebster@gmail.com>
2022-11-25 01:46:11 -0600

    Removal of editing total expense button

--- src/containers/TotalSum/TotalSum.js
+++ src/containers/TotalSum/TotalSum.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '../../components/Button/Button';
 import classes from './TotalSum.module.scss';
 
 const { totalContainer, number } = classes;
@@ -13,10 +12,6 @@ const TotalSum = ({ amount, edit }) => {
                 </p>
                 <span className={number}>${Math.round((amount * 100) / 100).toFixed(2)}</span>
             </div>
-            {/* <Button
-                type='nav'
-                action={edit}
-                title="Edit total amount" /> */}
         </>
     );
 };
