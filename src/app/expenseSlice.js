56f7cc685d529ceb372f117a63886282380569e2
Wesley <wesleyadwebster@gmail.com>
2022-12-03 03:38:00 -0600

    data from form can update redux state and display now

--- src/app/expenseSlice.js
+++ src/app/expenseSlice.js
@@ -2,18 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     expenses: [
-        // {
-        //     expenseName: 'Korean BBQ',
-        //     personBeingPaid: 'Claude',
-        //     cost: 140,
-        //     payers: ['Claude', 'Hilda'],
-        // },
-        // {
-        //     expenseName: 'Steakhouse',
-        //     personBeingPaid: 'Marianne',
-        //     cost: 1500.43,
-        //     payers: ['Marianne', 'Claude', 'Hilda', 'Ingrid', 'Huebert', 'Flayn', 'Byleth', 'Ferdinand', 'Victor'],
-        // },
+        {
+            expenseName: 'Korean BBQ',
+            personBeingPaid: 'Claude',
+            cost: 140,
+            payers: ['Claude', 'Hilda'],
+        },
+        {
+            expenseName: 'Steakhouse',
+            personBeingPaid: 'Marianne',
+            cost: 1500.43,
+            payers: ['Marianne', 'Claude', 'Hilda', 'Ingrid', 'Huebert', 'Flayn', 'Byleth', 'Ferdinand', 'Victor'],
+        },
         // {
         //     expenseName: 'Round of drinks at the Merchant',
         //     personBeingPaid: 'Hilda',
@@ -29,12 +29,7 @@ const expenseSlice = createSlice({
     initialState,
     reducers: {
         setExpenseBucket: (state, action) => {
-            state.expenses.push({
-                expenseName: action.payload,
-                personBeingPaid: action.payload,
-                cost: action.payload,
-                payers: action.payload,
-            });
+            state.expenses.push(action.payload);
         },
 
         addPersonToPayers: (state, action) => {
--- src/containers/Expenses/Expenses.js
+++ src/containers/Expenses/Expenses.js
@@ -6,12 +6,33 @@ import classes from './Expenses.module.scss';
 const { expenseGrid } = classes;
 
 const Expenses = () => {
-
+    let count = 0;
     const expenses = useSelector((state) => state.expense.expenses)
+    
+    
+    // Attempt 1: ForEash
+    // const newData = expenses.forEach(item => {
+    //     for (let key in item) {
+    //         // console.log(`${key}: ${item[key]}`)
+    //         // console.log(`${key}: ${item}`)
+    //     }
+    // });
+
+    // const newData = expenses.map(item => {
+    //     console.log(item)
+    // });
+
+    // Object.values(expenses).forEach((val) => {
+    //     console.log(val)
+    // });
+
+    console.log(expenses)
+
 
     return (
         <section className={expenseGrid}>
             {expenses?.map((activity) => {
+                console.log(activity)
                 return (
                     <ActivityItem
                         key={activity.expenseName}
@@ -19,6 +40,7 @@ const Expenses = () => {
                         personPaid={activity.personBeingPaid}
                         cost={activity.cost}
                         payers={activity.payers}
+                        payNames
                     />
                 )
             })}
